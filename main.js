1b7f1337cfc9e3b85a1f16837696c83ec3ed832b
Camille <106812932+camillinhaaaaa@users.noreply.github.com>
2024-07-25 20:27:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
-for (let i = 0; i < botoes.length; j++) {
-    botoes[i].onclick = function () {
+const textos = document.querySelectorAll(".aba-conteudo");
 
-        for (let j=0;j<botoes.length;j++){
+for (let i= 0;i < botoes.length;i++){
+    botoes[i].onclick = function (){
+
+        for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
-    };
+    }
+    
 }
 
 
